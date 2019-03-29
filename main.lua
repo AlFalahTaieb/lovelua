2e3b290c13d2c357816f1fc05a19c8c4a40702a1
AlFalah <taiebalfalah@gmail.com>
2019-03-29 17:49:42 +0100

    😍

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
-function love.draw()
-    love.graphics.print("Hello World!", 400, 300)
-    end
+-- function love.draw()
+--     love.graphics.print("Hello World!", 400, 300)
+--     end
+
+-- function calcul(a,b)
+--     return a+b
+-- end
+
+-- result=calcul(17,12)
+
+function intro(name,job,age)
+    print('Hi My Name : '..name.." I'm a "..job.." and i'm "..age..' years old.')
+end
+
+print(intro('Chdoula','devCowboy','26'))
