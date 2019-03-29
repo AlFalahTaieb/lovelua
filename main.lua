d1d729315091a0c063b7eb96b02e86013ef5a0ad
AlFalah <taiebalfalah@gmail.com>
2019-03-29 17:57:45 +0100

    😍

--- main.lua
+++ main.lua
@@ -8,8 +8,20 @@
 
 -- result=calcul(17,12)
 
-function intro(name,job,age)
-    print('Hi My Name : '..name.." I'm a "..job.." and i'm "..age..' years old.')
+-- function intro(name,job,age)
+--     print('Hi My Name : '..name.." I'm a "..job.." and i'm "..age..' years old.')
+-- end
+
+-- print(intro('Chdoula','devCowboy','26'))
+
+
+function love.load()
 end
 
-print(intro('Chdoula','devCowboy','26'))
+function love.update(dt)
+end
+
+function love.draw()
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", 300, 300, 50, 100)
+end
