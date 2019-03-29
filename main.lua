33a5a7934077aec31436541198c1883130c3da06
AlFalah <taiebalfalah@gmail.com>
2019-03-29 18:37:37 +0100

    😍

--- main.lua
+++ main.lua
@@ -14,14 +14,46 @@
 
 -- print(intro('Chdoula','devCowboy','26'))
 
-
 function love.load()
+    x = 300
+    y = 300
+    -- move = 600
+    drawRectangle()
+end
+
+function drawRectangle()
+    rectangle = {}
+    rectangle.width = 50
+    rectangle.height = 50
+    rectangle.x = 300
+    rectangle.y = 200
+    rectangle.speed=100
+end
+function drawCercle()
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle('fill', x, y, 50, 100)
+end
+
+function move(dt)
+    if love.keyboard.isDown('right') then
+        x = x + 100 * dt
+    end
+    if love.keyboard.isDown('left') then
+        x = x - 100 * dt
+    end
+    if love.keyboard.isDown('up') then
+        y = y - 100 * dt
+    end
+    if love.keyboard.isDown('down') then
+        y = y + 100 * dt
+    end
 end
 
 function love.update(dt)
+    -- move(dt)
+    rectangle.x = rectangle.x + rectangle.speed*dt
 end
 
 function love.draw()
-    love.graphics.setColor(1, 0, 0)
-    love.graphics.circle("fill", 300, 300, 50, 100)
+    love.graphics.rectangle('line',rectangle.x,rectangle.y,rectangle.height,rectangle.width)
 end
