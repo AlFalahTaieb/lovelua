f74ea88e199fa9ba0127a9cd9e30aaa88f75c794
AlFalah <taiebalfalah@gmail.com>
2019-03-31 12:36:17 +0100

    😍

--- entity.lua
+++ entity.lua
@@ -12,12 +12,19 @@ end
 function Entity:update(dt)
     self.x = self.x + self.xSpeed * dt
     self.y = self.y + self.ySpeed * dt
+    if self.y <= 0 then
+        self.y = 0
+        self.ySpeed = -self.ySpeed
+    elseif self.y + self.height>= 600 then
+        self.y = 600 - self.height
+        self.ySpeed = -self.ySpeed
+    endss
 end
 
 function Entity:draw()
     r = love.math.random()
     g = love.math.random()
     b = love.math.random()
-    love.graphics.setColor(r, g, b)
+
     love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
 end
--- game.lua
+++ game.lua
@@ -6,14 +6,17 @@ self.padDroite = Pad()
 self.padDroite.x = 740
 self.padDroite.keyUp="z"
 self.padDroite.keyDown="s"
+self.ball = Ball()
 end
 
 function Game:update(dt)
 self.padDroite:update(dt)
 self.padGauche:update(dt)
+self.ball:update(dt)
 end
 
 function Game:draw()
   self.padGauche:draw()
   self.padDroite:draw()
+  self.ball:draw()
 end
